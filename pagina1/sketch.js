0a0708c824fcaa0d0db7b02a916f1012abfc7230
ceci <cecilia.perez.v@mail.pucv.cl>
2020-09-21 23:56:42 -0300

    arreglando cositas

--- pagina1/sketch.js
+++ pagina1/sketch.js
@@ -2,5 +2,3 @@ function classToggle() {
   var el = document.querySelector('.icon-cards__content');
   el.classList.toggle('step-animation');
 }
-
-document.querySelector('#toggle-animation').addEventListener('click', classToggle);
