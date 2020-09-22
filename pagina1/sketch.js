626152ad3a354f51c36f69702113b63ec789801f
ceci <cecilia.perez.v@mail.pucv.cl>
2020-09-21 23:45:00 -0300

    re-toques

--- pagina1/sketch.js
+++ pagina1/sketch.js
@@ -1,3 +1,6 @@
-$('.carousel').carousel({
-  interval: 100
-})
+function classToggle() {
+  var el = document.querySelector('.icon-cards__content');
+  el.classList.toggle('step-animation');
+}
+
+document.querySelector('#toggle-animation').addEventListener('click', classToggle);
